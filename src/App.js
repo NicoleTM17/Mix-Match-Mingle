c07c8399ed7c77c91e215fcd5c2b776d69755c7c
Nicole Moncrieffe <nicoletmoncrieffe@gmail.com>
2023-08-11 11:48:47 +0100

    first commit. Put in filler content

--- src/App.js
+++ src/App.js
@@ -1,23 +1,68 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <img className='logo' src='images/logo.png' alt='logo'/>
+
+      <div className='search-bar'>
+        <form className='searchbar'>
+          <input type='text' className='search' style={{fontStyle: 'italic'}} placeholder='Enter a cocktail of your choice'/>
+          <input type='submit' className='submit-btn' value='Enter'/>
+        </form>
+      </div>
+
+      <div className='search-results-section'>
+        <p className='results results-hidden'>There are no results matching your search</p>
+
+
+        <div className='cocktail-card' style={{backgroundImage: "url('images/cocktails.png')", backgroundPosition: 'bottom right', backgroundRepeat: 'no-repeat', backgroundSize: '30%'}}>
+          <h1 className='cocktail-title'>Sex on the beach recipe</h1>
+
+          <span className='instructions'>
+            <h3 className='instructions-title'>Instructions:</h3>
+            <p className='instructions-text'>Build all ingredients in a highball glass filled with ice. Garnish with orange slice.</p>
+          </span>
+
+          <span className='ingredients'>
+            <h3 className='ingredients-title'>Ingredients:</h3>
+
+            <ul className='ingredients-list'>
+              <li className='ingredient'>4 cl Vodka</li>
+              <li className='ingredient'>2 cl Peach schnapps</li>
+              <li className='ingredient'>4 cl Orange juice</li>
+              <li className='ingredient'>4 cl cranberry juice</li>
+            </ul>
+
+          </span>
+
+        </div>
+      </div>
+      <footer>© Copyright 2023 Nicole Moncrieffe</footer>
+
+
     </div>
   );
 }
